806729854f95d01102dcce0a239d8dc52130629f
Art2B <arthur.battut@gmail.com>
2018-06-23 10:59:27 +0200

    Add App component

---
+++ src/components/app/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const AppWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+`
+
+const App = ({}) => (
+  <AppWrapper>
+    <p>Put app here</p>
+  </AppWrapper>
+)
+
+export default App
